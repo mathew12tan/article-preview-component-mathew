447599b58fb562fb701b4ce8d40b152b9be55574
mathew tan <mathew12tan@gmail.com>
2021-05-17 09:02:03 +0800

    initial commit

---
+++ main.js
@@ -0,0 +1,26 @@
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+const date = new Date();
+const avatarDate = document.querySelector(".avatar-date");
+avatarDate.textContent = `${date.getDate()} ${months[date.getMonth()]} ${date.getFullYear()}`;
+
+const shareContainer = document.querySelector(".share-container");
+const shareBtns = document.querySelectorAll(".shareBtn");
+
+shareBtns.forEach((shareBtn) => {
+  shareBtn.addEventListener("click", function () {
+    shareContainer.classList.toggle("share-container-toggle");
+  });
+});
